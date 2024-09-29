d5c155be9167efd4d3ff3fc7bde0cf51ee3e1bb9
vkdevz <144937707+vkdevz@users.noreply.github.com>
2024-09-30 00:40:12 +0530

    header and footer using js
    Signed-off-by: vkdevz <144937707+vkdevz@users.noreply.github.com>

---
+++ e_commerce_frontend/js/scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Item added to cart');
+        });
+    });
+});
