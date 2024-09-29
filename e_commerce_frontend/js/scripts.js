8c7ff3729e8a7629e8f7f61a50e5f6c619cf94c7
vkdevz <144937707+vkdevz@users.noreply.github.com>
2024-09-30 00:46:20 +0530

    Delete e_commerce_frontend directory
    Signed-off-by: vkdevz <144937707+vkdevz@users.noreply.github.com>

--- e_commerce_frontend/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const addToCartButtons = document.querySelectorAll('.add-to-cart');
-
-    addToCartButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Item added to cart');
-        });
-    });
-});
